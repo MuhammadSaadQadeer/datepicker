cad6ee679e5c74a03aa2742d729e78ff8b071627
saad <saadqadeer59@gmail.com>
2020-09-09 09:28:49 +0500

    chore: add basic calendar sekeleton

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React from "react";
 import "./App.css";
 import { getCalendarData } from "./utils";
+import { isEqual, format } from "date-fns";
 
 function App() {
   return (
-    <div className="App">
+    <div className="calendar-container">
+      {<div style={{color:"black", textAlign:"center"}}>{getCalendarData().month}</div>}
+
       <div style={{ display: "flex", flexDirection: "row"}}>
         {["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"].map((item) => {
-          return <div style={{padding: 5}}>{item}</div>;
+          return <div style={{padding: 4}}>{item}</div>;
         })}
       </div>
-      {getCalendarData().map((item) => {
+      {getCalendarData().calendar.map((item) => {
         return (
-          <div style={{ display: "flex", flexDirection: "row" , padding:20}}>
+          <div style={{ display: "flex", flexDirection: "row"}}>
+            {console.log("foo",item)}
             {item.map((col) => {
               return (
-                <div style={{ padding: 10 }}>{`${col.date}`} </div>
+                <div style={{padding:10, color:!col.current ? 'lightgray':'black', backgroundColor: col.today ? 'aliceblue':''}}>{ `${col.date}`}
+                
+                 </div>
               );
             })}
             <br />
--- src/utils.js
+++ src/utils.js
@@ -11,14 +11,33 @@ import {
     isFriday,
     isSaturday,
     isSunday,
-    isThisMonth
+    isThisMonth,
+    isToday
   } from "date-fns";
   
-  const rows = range(6);
+  const rows = range(5);
   const cols = range(7);
   
   function range(n) {
     return [...Array(n).keys()];
+  }
+
+  function getMonthName(number){
+    const monthMap = {
+      0: "January",
+      1:"February",
+      2:"March",
+      3:"April",
+      4:"May",
+      5:"June",
+      6:"July",
+      7:"August",
+      8:"September",
+      9:"October",
+      10:"November",
+      11:"December"
+    }
+    return monthMap[number]
   }
   
   function getDayName(date) {
@@ -62,7 +81,18 @@ import {
           if(isThisMonth(curDate)){
             week.push({
                 day: getDayName(curDate),
-                date: format(curDate, "dd")
+                date: format(curDate, "dd"),
+                fulldate:curDate,
+                current:true,
+                today: isToday(curDate)
+              });   
+          }else {
+            week.push({
+                day: getDayName(curDate),
+                date: format(curDate, "dd"),
+                fulldate:curDate,
+                current:false,
+                today: isToday(curDate)
               });
           }
        
@@ -71,8 +101,8 @@ import {
   
       matrix.push(week);
     });
-  
-    return matrix;
+    console.log(year, getMonthName(month), weekStartsOn)
+    return {calendar:matrix, year, month:getMonthName(month)};
   }
   
   
