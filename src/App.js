81913b4d417d44b55b6d77eaa255539a8f191030
saad <saadqadeer59@gmail.com>
2020-09-14 17:49:55 +0500

    chore: add styles for hover and selected day

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { useState } from "react";
 import "./App.css";
 import { getCalendarData, getMonthName } from "./utils";
-import { isEqual, format, getMonth, getYear ,addYears, subYears} from "date-fns";
+import {
+  isEqual,
+  format,
+  getMonth,
+  getYear,
+  addYears,
+  subYears,
+  getDay,
+} from "date-fns";
 
 function App() {
   const [month, setMonth] = useState(getMonth(new Date()));
   const [year, setYear] = useState(getYear(new Date()));
-  
+  const [selected, setSelected] = useState(format(new Date(), "dd"));
   const incrementMonth = () => {
     let monthNumber = month;
     monthNumber++;
     setMonth(monthNumber);
     if (monthNumber > 11) {
-      setYear(getYear(addYears(new Date(year, month, 1), 1)))
+      setYear(getYear(addYears(new Date(year, month, 1), 1)));
       setMonth(0);
     }
   };
@@ -22,73 +30,69 @@ function App() {
     monthNumber--;
     setMonth(monthNumber);
     if (monthNumber < 0) {
-      setYear(getYear(subYears(new Date(year, month, 1), 1)))
+      setYear(getYear(subYears(new Date(year, month, 1), 1)));
       setMonth(11);
     }
   };
 
-  // const incrementYear = () => {
-  //   // if month is greater than 11 than we will add year into current year
-
-  //   if(month == 11){
-  //     console.log(new Date(year))
-  //      setYear(getYear(addYears(new Date(year, month, 1), 1)))
-  //   }
-
-  //   if(month < 0){
-  //     console.log(new Date(year))
-  //      setYear(getYear(subYears(new Date(year, month, 1), 1)))
-  //   }
-  // }
-
-  // const decrementYear = () => {
-  //   // if month is less than 0 than we will subtract year from current year
-  // }
   return (
+    <div>
     <div className="calendar-container">
       <div
         style={{
           display: "flex",
           justifyContent: "space-between",
           alignItems: "center",
-          width:"15%"
+          width: "15%",
+          marginBottom:10
         }}
       >
-        <button onClick={decrementMonth}> {"<"} </button>
         <div style={{ color: "black", textAlign: "center" }}>
           {getMonthName(month)} {year}
         </div>
-        <button onClick={incrementMonth}>{">"}</button>
+        <div>
+          <button style={{background:"transparent", borderRadius:4}} onClick={decrementMonth}> {"<"} </button>
+            &nbsp;
+          <button style={{background:"transparent", borderRadius:4}} onClick={incrementMonth}>{">"}</button>
+        </div>
       </div>
+
 
       <div style={{ display: "flex", flexDirection: "row" }}>
         {["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"].map((item) => {
-          return <div style={{ padding: 4 }}>{item}</div>;
+          return <div style={{ display:"flex", justifyContent:"center", alignItems:"center", textAlign:"center", padding: 18,  width:10,
+          height:10 }}>{item}</div>;
         })}
       </div>
-      {getCalendarData({ year: year,
-      month: month,
-      weekStartsOn: 0}).calendar.map((item) => {
+      {getCalendarData({
+        year: year,
+        month: month,
+        weekStartsOn: 0,
+      }).calendar.map((item) => {
         return (
-          <div style={{ display: "flex", flexDirection: "row" }}>
+          <div style={{ display: "flex", flexDirection: "row"}}>
             {item.map((col) => {
               return (
                 <div
+                  id="date-labels"
+                  onClick={()=> setSelected(col.date)}
                   style={{
-                    padding: 10,
+                    display:"flex", justifyContent:"center", alignItems:"center",
+                    padding: 18,
                     color: !col.current ? "lightgray" : "black",
-                    backgroundColor: col.today ? "aliceblue" : "",
-                    cursor:"pointer",
+                    backgroundColor: selected===col.date ? "aliceblue" : "",
+                    width:10,
+                    height:10
                   }}
                 >
                   {`${col.date}`}
                 </div>
               );
             })}
-            <br />
           </div>
         );
       })}
+    </div>
     </div>
   );
 }
