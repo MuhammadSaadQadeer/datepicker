d30a2afe1d60be05548fbe11abca37f7e764b0b3
saad <saadqadeer59@gmail.com>
2020-09-16 09:20:52 +0500

    refactor: code refactoring and ws settings updates

--- src/App.js
+++ src/App.js
@@ -1,100 +1,20 @@
 import React, { useState } from "react";
-import "./App.css";
-import { getCalendarData, getMonthName } from "./utils";
-import {
-  isEqual,
-  format,
-  getMonth,
-  getYear,
-  addYears,
-  subYears,
-  getDay,
-} from "date-fns";
+import { usePopper } from "react-popper";
+import Calendar from "./Calendar";
 
-function App() {
-  const [month, setMonth] = useState(getMonth(new Date()));
-  const [year, setYear] = useState(getYear(new Date()));
-  const [selected, setSelected] = useState(format(new Date(), "dd"));
-  const incrementMonth = () => {
-    let monthNumber = month;
-    monthNumber++;
-    setMonth(monthNumber);
-    if (monthNumber > 11) {
-      setYear(getYear(addYears(new Date(year, month, 1), 1)));
-      setMonth(0);
-    }
-  };
-
-  const decrementMonth = () => {
-    let monthNumber = month;
-    monthNumber--;
-    setMonth(monthNumber);
-    if (monthNumber < 0) {
-      setYear(getYear(subYears(new Date(year, month, 1), 1)));
-      setMonth(11);
-    }
-  };
+const App = () => {
+  const [referenceElement, setReferenceElement] = useState(null);
+  const [popperElement, setPopperElement] = useState(null);
+  const [arrowElement, setArrowElement] = useState(null);
+  const { styles, attributes } = usePopper(referenceElement, popperElement, {
+    modifiers: [{ name: "arrow", options: { element: arrowElement } }],
+  });
 
   return (
-    <div>
-    <div className="calendar-container">
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          width: "15%",
-          marginBottom:10
-        }}
-      >
-        <div style={{ color: "black", textAlign: "center" }}>
-          {getMonthName(month)} {year}
-        </div>
-        <div>
-          <button style={{background:"transparent", borderRadius:4}} onClick={decrementMonth}> {"<"} </button>
-            &nbsp;
-          <button style={{background:"transparent", borderRadius:4}} onClick={incrementMonth}>{">"}</button>
-        </div>
-      </div>
-
-
-      <div style={{ display: "flex", flexDirection: "row" }}>
-        {["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"].map((item) => {
-          return <div style={{ display:"flex", justifyContent:"center", alignItems:"center", textAlign:"center", padding: 18,  width:10,
-          height:10 }}>{item}</div>;
-        })}
-      </div>
-      {getCalendarData({
-        year: year,
-        month: month,
-        weekStartsOn: 0,
-      }).calendar.map((item) => {
-        return (
-          <div style={{ display: "flex", flexDirection: "row"}}>
-            {item.map((col) => {
-              return (
-                <div
-                  id="date-labels"
-                  onClick={()=> setSelected(col.date)}
-                  style={{
-                    display:"flex", justifyContent:"center", alignItems:"center",
-                    padding: 18,
-                    color: !col.current ? "lightgray" : "black",
-                    backgroundColor: selected===col.date ? "aliceblue" : "",
-                    width:10,
-                    height:10
-                  }}
-                >
-                  {`${col.date}`}
-                </div>
-              );
-            })}
-          </div>
-        );
-      })}
-    </div>
-    </div>
+    <>
+      <Calendar />
+    </>
   );
-}
+};
 
 export default App;
--- src/utils.js
+++ src/utils.js
@@ -1,108 +1,111 @@
 import {
-    getYear,
-    getMonth,
-    addDays,
-    startOfWeek,
-    format,
-    isMonday,
-    isTuesday,
-    isWednesday,
-    isThursday,
-    isFriday,
-    isSaturday,
-    isSunday,
-    isThisMonth,
-    isToday
-  } from "date-fns";
-  
-  const rows = range(5);
-  const cols = range(7);
-  
-  function range(n) {
-    return [...Array(n).keys()];
-  }
+  getYear,
+  getMonth,
+  addDays,
+  startOfWeek,
+  format,
+  isMonday,
+  isTuesday,
+  isWednesday,
+  isThursday,
+  isFriday,
+  isSaturday,
+  isSunday,
+  isThisMonth,
+  isToday,
+} from "date-fns";
 
-  export function getMonthName(number){
-    const monthMap = {
-      0: "January",
-      1:"February",
-      2:"March",
-      3:"April",
-      4:"May",
-      5:"June",
-      6:"July",
-      7:"August",
-      8:"September",
-      9:"October",
-      10:"November",
-      11:"December"
-    }
-    return monthMap[number]
+const rows = range(5);
+const cols = range(7);
+
+function range(n) {
+  return [...Array(n).keys()];
+}
+
+export function getMonthName(number) {
+  const monthMap = {
+    0: "January",
+    1: "February",
+    2: "March",
+    3: "April",
+    4: "May",
+    5: "June",
+    6: "July",
+    7: "August",
+    8: "September",
+    9: "October",
+    10: "November",
+    11: "December",
+  };
+  return monthMap[number];
+}
+
+function getDayName(date) {
+  if (isMonday(date)) {
+    return "MO";
   }
-  
-  function getDayName(date) {
-    if (isMonday(date)) {
-      return "MO";
-    }
-    if (isTuesday(date)) {
-      return "TU";
-    }
-    if (isWednesday(date)) {
-      return "WED";
-    }
-    if (isThursday(date)) {
-      return "THU";
-    }
-    if (isFriday(date)) {
-      return "FRI";
-    }
-    if (isSaturday(date)) {
-      return "SAT";
-    }
-    if (isSunday(date)) {
-      return "SUN";
-    }
+  if (isTuesday(date)) {
+    return "TU";
   }
-  
-  export function getCalendarData(
-    { year, month, weekStartsOn } = {
-      year: getYear(new Date()),
-      month: getMonth(new Date()),
-      weekStartsOn: 0
-    }
-  ) {
-    const matrix = [];
-    const date = new Date(year, month);
-    let curDate = startOfWeek(date, { weekStartsOn });
-  
-    rows.forEach((row) => {
-      const week = [];
-      cols.forEach((col) => {
-          if(isThisMonth(curDate)){
-            week.push({
-                day: getDayName(curDate),
-                date: format(curDate, "dd"),
-                fulldate:curDate,
-                current:true,
-                today: isToday(curDate)
-              });   
-          }else {
-            week.push({
-                day: getDayName(curDate),
-                date: format(curDate, "dd"),
-                fulldate:curDate,
-                current:false,
-                today: isToday(curDate)
-              });
-          }
-       
-        curDate = addDays(curDate, 1);
-      });
-  
-      matrix.push(week);
+  if (isWednesday(date)) {
+    return "WED";
+  }
+  if (isThursday(date)) {
+    return "THU";
+  }
+  if (isFriday(date)) {
+    return "FRI";
+  }
+  if (isSaturday(date)) {
+    return "SAT";
+  }
+  if (isSunday(date)) {
+    return "SUN";
+  }
+}
+
+export function getCurrentDateWithWeek(year, month, weekStartsOn) {
+  const date = new Date(year, month);
+  return startOfWeek(date, { weekStartsOn });
+}
+
+export function getCalendarData(
+  { year, month, weekStartsOn } = {
+    year: getYear(new Date()),
+    month: getMonth(new Date()),
+    weekStartsOn: 0,
+  }
+) {
+  const matrix = [];
+  const date = new Date(year, month);
+  let curDate = startOfWeek(date, { weekStartsOn });
+
+  rows.forEach((row) => {
+    const week = [];
+    cols.forEach((col) => {
+      if (isThisMonth(curDate)) {
+        week.push({
+          day: getDayName(curDate),
+          date: format(curDate, "dd"),
+          fulldate: curDate,
+          current: true,
+          today: isToday(curDate),
+        });
+      } else {
+        week.push({
+          day: getDayName(curDate),
+          date: format(curDate, "dd"),
+          fulldate: curDate,
+          current: false,
+          today: isToday(curDate),
+        });
+      }
+
+      curDate = addDays(curDate, 1);
     });
-    // console.log(year, getMonthName(month), weekStartsOn)
-    return {calendar:matrix, year, month:getMonthName(month)};
-  }
-  
-  
+
+    matrix.push(week);
+  });
+  // console.log(year, getMonthName(month), weekStartsOn)
+  return { calendar: matrix, year, month: getMonthName(month) };
+}
